f7d49bddcba8cb4f9baff540b6a95ea71b42cfb7
Santos123-Sys <115656988+Santos123-Sys@users.noreply.github.com>
2026-09-08 17:38:29 -0300

    Add investment control center

---
+++ src/app/governance/page.tsx
@@ -0,0 +1,7 @@
+import { GovernanceDashboard } from '@/components/governance-dashboard';
+
+export const dynamic = 'force-dynamic';
+
+export default function GovernancePage() {
+  return <GovernanceDashboard />;
+}
